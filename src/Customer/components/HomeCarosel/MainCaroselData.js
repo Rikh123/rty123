2d654549bbbb126d81d5b845c764eded4045bc51
Rikh123 <159327841+Rikh123@users.noreply.github.com>
2024-08-18 17:33:48 +0530

    Update MainCaroselData.js

--- src/Customer/components/HomeCarosel/MainCaroselData.js
+++ src/Customer/components/HomeCarosel/MainCaroselData.js
@@ -1,18 +1,18 @@
 export const mainCarouselData = [
     {
-        image: "https://n.nordstrommedia.com/id/sr3/16554823-8eb1-4c73-aec9-8c22e59e34ea.jpeg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1196",
+        image: "https://th.bing.com/th/id/OIP.HUgVtU44BeGoKTHhd9C-SAHaJd?rs=1&pid=ImgDetMain",
         path: "/women/clothing/lengha_choli"
     },
     {
-        image: "",
+        image: "https://images.asos-media.com/products/adidas-football-tiro-23-t-shirt-in-black/203429636-1-black?$n_640w$&wid=513&fit=constrain",
         path: "/women/clothing/women_dress"
     },
     {
-        image: "",
+        image: "https://s7d5.scene7.com/is/image/UrbanOutfitters/54219811_001_b",
         path: "/women/clothing/women_dress"
     },
     {
-        image: "",
+        image: "https://th.bing.com/th/id/OIP.ebAHcDVkmVFMkN0UT8tJtQHaKr?w=1220&h=1760&rs=1&pid=ImgDetMain",
         path: "/women/clothing/women_saree"
     }
 ];
